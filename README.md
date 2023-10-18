83090aa01d7d7f894a3b708744d4240de502afe7
Abdul Aziz <90431550+Gure-san@users.noreply.github.com>
2023-10-18 04:23:33 +0000

    mengubah content

--- README.md
+++ README.md
@@ -62,6 +62,3 @@ All commands are run from the root of the project, from a terminal:
 | `npm run astro ...`    | Run CLI commands like `astro add`, `astro check` |
 | `npm run astro --help` | Get help using the Astro CLI                     |
 
-## 👀 Want to learn more?
-
-Feel free to check [Astro documentation](https://docs.astro.build) or jump into Astro [Discord server](https://astro.build/chat).
--- src/data/info.ts
+++ src/data/info.ts
@@ -1,88 +1,75 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
+  name: "Abdul Aziz",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  about: `
+
+  Saya adalah seorang web developer yang
+berkomitmen tinggi untuk menciptakan pengalaman
+pengguna yang menarik dan fungsional. Saya juga
+telah memperoleh pengalaman berharga sebagai
+seorang freelance web developer, yang telah
+memungkinkan saya untuk mengasah keterampilan
+dan pemahaman dalam dunia pemrograman web.
+
+<br/>
+<br/>
+
+Dalam bidang ini, saya telah memperdalam pengetahuan
+berbagai bahasa pemrograman dan berbagai teknologi
+serta alat pengembangan web yang relevan, termasuk HTML, CSS, Javascript, Typescript, Node.js, React, Astro, Next.js. 
+
+<br/>
+<br/>
+
+Selain itu, saya juga memiliki pengalaman yang kuat dalam
+mengembangkan situs web yang responsif dan ramah
+pengguna. Selalu up-to-date dengan perkembangan
+teknologi terbaru, saya selalu berusaha memberikan
+solusi, menggabungkan kreativitas dan keahlian teknis
+untuk mencapai hasil yang terbaik.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
+      name: "Freelancer",
+      location: "Remote",
+      startDate: "Jun 2023",
+      endDate: "Sekarang",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "- Mengerjakan project aplikasi desa.",
+        "- Membuat component library (Design system) untuk kebutuhan project.",
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "Sekretariat Presiden (Magang)",
+      location: "Jl. Veteran No. 16 Jakarta Pusat",
+      startDate: "Feb 2022",
+      endDate: "Dec 2022",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
-    },
-
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "✓ Mengelola dan mengoprasikan software streaming",
+        "✓ Mempelajari penggunaan wordprass dan alat / teknologi pengembangan web",
+        "✓ Membuat aplikasi sederhana",
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
+      name: "SMK Negeri 4 Kota Malang",
+      location: "Jl. Tanimbar No.22, Kasin, Kec. Klojen, Kota Malang",
+      startDate: "2020",
+      endDate: "2023",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "Jurusan Rekayasa Perangkat Lunak (RPL)",
       ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
     github: "https://github.com/uzzii-21",
     email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
   },
 
   projects: [
